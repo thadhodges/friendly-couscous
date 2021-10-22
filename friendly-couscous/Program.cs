174b4b23a7987fe9dd52eaa4fdc97d847cd65f16
Thad <thadisfree@gmail.com>
2021-10-22 08:19:37 -0500

    initial commit

---
+++ friendly-couscous/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace friendly_couscous
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
